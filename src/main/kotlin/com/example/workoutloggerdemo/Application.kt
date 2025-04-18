7c4180441024861c537cb1f8de5d7f90d5eb0bae
antonarhipov <ant.arhipov@gmail.com>
2025-04-18 23:44:38 +0200

    Rename main application class for consistency
    The main application class was renamed from `WorkoutLoggerDemoApplication` to `Application`. This improves naming simplicity and aligns with conventional naming practices for Spring Boot applications. All references were updated accordingly.

--- src/main/kotlin/com/example/workoutloggerdemo/Application.kt
+++ src/main/kotlin/com/example/workoutloggerdemo/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class WorkoutLoggerDemoApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<WorkoutLoggerDemoApplication>(*args)
+    runApplication<Application>(*args)
 }
